b54410ce88ebd53cb72a2873f9c2a29d38f92deb
Noah Krieger <noahkrieger@gmail.com>
2025-11-04 20:11:39 -0500

    froze docker tags

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-docker buildx build --platform linux/amd64,linux/arm64 -t kasbench/globeco-confirmation-service:latest --push .
+docker buildx build --platform linux/amd64,linux/arm64 \
+ -t kasbench/globeco-confirmation-service:latest \
+-t kasbench/globeco-confirmation-service:1.0.0 \
+ --push .
--- kbuild.sh
+++ kbuild.sh
@@ -1,2 +1,6 @@
-docker buildx build --platform linux/amd64,linux/arm64 -t kasbench/globeco-confirmation-service:latest --push .
+docker buildx build --platform linux/amd64,linux/arm64 \
+--tag kasbench/globeco-confirmation-service:latest \
+--tag kasbench/globeco-confirmation-service:1.0.0 \
+--file Dockerfile \
+--push .
 kubectl rollout restart deployment globeco-confirmation-service   
